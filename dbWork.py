4ad190853a3b8eea3968520de33357cb83e447de
CBHue <CBHue@users.noreply.github.com>
2019-03-05 19:29:46 -0500

    Update dbWork.py

--- dbWork.py
+++ dbWork.py
@@ -83,6 +83,94 @@ def db_checkFile(c, hashFile, workingFile, LogFile):
 		printP("Checking if Hashes are in DB")
 		with open(workingFile, mode = 'a', encoding='utf-8') as wFile:
 			with open(LogFile, mode = 'a', encoding='utf-8') as oFile:
+				oFile.write("Hashes in DB\n")
+				oFile.write("***************************************************\n")
+				with open(hashFile, "r",encoding='utf-8', errors='ignore') as HashReader:
+					for hash in HashReader:
+						hash = hash.strip()
+						r = db_search(c, hash)
+						# Hash not in DB
+						if type(r) is str:
+							wFile.write(hash + "\n")
+						# Hash is already cracked
+						else:
+							password = hash + ":" + ', ,'.join([str(i[0]) for i in r]) + "\n"
+							oFile.write(str(password))
+						wFile.flush()
+						oFile.flush()
+		wFile.close()
+		oFile.close()
+	else:
+		printR("Issues with the hashFile ...")		
+
+def db_readFile(conn, fileName):
+	if fileName:
+		printP("Adding potFile to DB: " + fileName)
+		try:
+			c = conn.cursor()
+			c.execute("BEGIN")
+			with open(fileName, "r",encoding='utf-8', errors='ignore') as HashReader:
+				for row in HashReader:
+					sL,sR=row.split(':', 1)
+					c.execute("INSERT OR IGNORE INTO hashes VALUES (?,?)", [sL.strip(),sR.strip()])
+			conn.commit()
+
+		except IOError:
+			printR("Could not read file:"), fileName
+	else:
+		printR("Issues with " + fileName + " ...")
+
+def db_configure(dataDIR):
+	try: 
+	    os.makedirs(dataDIR)
+	except OSError:
+	    if not os.path.isdir(dataDIR):
+	        raise
+
+def start():
+	t = datetime.now()
+	printP("Starting Time " + str(t))
+	return t
+
+def endT(t):
+	tE = datetime.now() - t 
+	printP('Elapsed Time (hh:mm:ss.ms) {}'.format(tE))
+
+
+	except Exception:
+		printR("Could not connect to database")
+		raise SystemExit
+
+def db_close(conn):
+	conn.close()
+
+def db_search(c, hash):
+	hash = hash.strip()
+	cursor = c.cursor()
+	query = "SELECT password FROM hashes WHERE hash = '%s'" % hash
+	cursor.execute(query)
+	r = cursor.fetchall()
+	cursor.close()
+	if r:
+		return r
+	else:
+		return hash
+
+def db_getHashCount(conn):
+	printP("Checking DB for Hash count")
+	conn.execute('pragma journal_mode=wal')
+	c = conn.cursor()
+	c.execute('SELECT count(*) from hashes') 
+	r = c.fetchall()
+	c.close()
+	count = ', ,'.join([str(i[0]) for i in r])
+	printP("Hash Count  : " + locale.format("%d", int(count), grouping=True))
+
+def db_checkFile(c, hashFile, workingFile, LogFile):
+	if hashFile:
+		printP("Checking if Hashes are in DB")
+		with open(workingFile, mode = 'a', encoding='utf-8') as wFile:
+			with open(LogFile, mode = 'a', encoding='utf-8') as oFile:
 				oFile.write("Hashes in DB/n")
 				oFile.write("***************************************************/n")
 				with open(hashFile, "r",encoding='utf-8', errors='ignore') as HashReader:
